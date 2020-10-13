ad8ad778edcf6a6a07b52203a19f4a74af28a36a
huhengtong <758031719@qq.com>
2020-10-13 19:12:48 +0800

    Add files via upload

---
+++ mean_teacher/datasets.py
@@ -0,0 +1,82 @@
+import torchvision.transforms as transforms
+
+from . import data
+from .utils import export
+
+
+@export
+def imagenet():
+    channel_stats = dict(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+    train_transformation = data.TransformTwice(transforms.Compose([
+        transforms.RandomRotation(10),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+        transforms.ToTensor(),
+        transforms.Normalize(**channel_stats)
+    ]))
+    eval_transformation = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(**channel_stats)
+    ])
+
+    return {
+        'train_transformation': train_transformation,
+        'eval_transformation': eval_transformation,
+        #'datadir': 'data-local/images/ilsvrc2012/',
+        'num_classes': 1000
+    }
+
+
+@export
+def miniimagenet():
+    mean_pix = [x / 255.0 for x in [120.39586422, 115.59361427, 104.54012653]]
+    std_pix = [x / 255.0 for x in [70.68188272, 68.27635443, 72.54505529]]
+
+    channel_stats = dict(mean=mean_pix,
+                         std=std_pix)
+
+    train_transformation = data.TransformTwice(transforms.Compose([
+        transforms.RandomRotation(10),
+        transforms.RandomCrop(84, padding=8),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(**channel_stats)
+    ]))
+
+    eval_transformation = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(**channel_stats)
+    ])
+
+    return {
+        'train_transformation': train_transformation,
+        'eval_transformation': eval_transformation,
+        #'datadir': 'data-local/images/ilsvrc2012/',
+        'num_classes': 100
+    }
+
+
+@export
+def cifar100():
+    channel_stats = dict(mean=[0.4914, 0.4822, 0.4465],
+                         std=[0.2470,  0.2435,  0.2616])
+    train_transformation = data.TransformTwice(transforms.Compose([
+        data.RandomTranslateWithReflect(4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(**channel_stats)
+    ]))
+    eval_transformation = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(**channel_stats)
+    ])
+
+    return {
+        'train_transformation': train_transformation,
+        'eval_transformation': eval_transformation,
+        'num_classes': 100
+    }
